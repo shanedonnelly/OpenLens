4cbb3d9aecc267db7046d6c587df3d9f6b2087f3
shane donnelly <shane@donnelly>
2025-05-21 16:16:53 +0300

    Small change

--- secret_key.py
+++
@@ -1,8 +0,0 @@
-"""
-Store API keys and other secrets
-NOTE: Do not commit this file to version control!
-"""
-
-# OpenRouter API key or any other API key
-# changed to new key
-API_KEY = "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"
